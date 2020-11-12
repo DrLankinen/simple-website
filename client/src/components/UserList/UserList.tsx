c6b9840b90ddd21b3c9e501fb94da72fcc6da66f
Elias Lankinen <elias@lankinen.xyz>
2020-11-12 17:42:36 +0200

    filter based verification functionality

--- client/src/components/UserList/UserList.tsx
+++ client/src/components/UserList/UserList.tsx
@@ -25,26 +25,42 @@ const GET_ROCKET_INVENTORY = gql`
 
 const RESULTS_PER_PAGE: number = 23
 
+enum VerifiedFilter {
+  NONE,
+  VERIFIED,
+  NONVERIFIED
+}
+
 export function UserList() {
   const { loading, data } = useQuery<UserData, UserVars>(
     GET_ROCKET_INVENTORY,
     {}
   );
 
-  const [searchQuery, setSearchQuery] = useState("")
+  const [searchQuery, setSearchQuery] = useState<string>("")
+  const [filterVerification, setFilterVerification] = useState<VerifiedFilter>(VerifiedFilter.NONE)
 
   const userFilter = (user: User) => {
     const name = user.name.toLowerCase()
     const shortBio = user.shortBio.toLowerCase()
     const search = searchQuery.toLowerCase()
-    return name.includes(search) || shortBio.includes(search)
+    const queryFilter = name.includes(search) || shortBio.includes(search)
+
+    switch (filterVerification) {
+      case VerifiedFilter.VERIFIED:
+        return queryFilter && user.isVerified
+      case VerifiedFilter.NONVERIFIED:
+        return queryFilter && !user.isVerified
+      default:
+        return queryFilter
+    }
   }
   const [filteredUsers, setFilteredUsers] = useState<User[]>([])
   useEffect(() => {
     if (data) {
       setFilteredUsers(data.Users.filter(userFilter))
     }
-  }, [data, searchQuery])
+  }, [data, searchQuery, filterVerification])
 
   const [maxPage, setMaxPage] = useState<number>(1)
   useEffect(() => {
@@ -70,6 +86,25 @@ export function UserList() {
       ) : (
           <>
             <input type="text" value={searchQuery} placeholder="Search" onChange={(event) => setSearchQuery(event.target.value)} />
+
+            <select value={filterVerification} onChange={event => {
+              switch (event.target.value) {
+                case "0":
+                  setFilterVerification(VerifiedFilter.NONE)
+                  break
+                case "1":
+                  setFilterVerification(VerifiedFilter.VERIFIED)
+                  break
+                case "2":
+                  setFilterVerification(VerifiedFilter.NONVERIFIED)
+                  break
+              }
+            }}>
+              <option value={VerifiedFilter.NONE}>None</option>
+              <option value={VerifiedFilter.VERIFIED}>Verified</option>
+              <option value={VerifiedFilter.NONVERIFIED}>Non-Verified</option>
+            </select>
+
             <List users={filteredUsers.slice((page - 1) * RESULTS_PER_PAGE, page * RESULTS_PER_PAGE)} />
             <Pagination setPage={setPage} currentPage={page} maxPage={maxPage} />
           </>
